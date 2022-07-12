bb0fb54d660922444bf5f50d3d7ca2aae85b2d91
Olivier Adam <olivier@getunlatch.com>
2022-07-12 08:15:48 +0200

    replace words by ints for better efficiency

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,19 @@ fn rate_word(solution: &Word, guess: &Word) -> Score {
     return result;
 }
 
+fn score_to_int(s: &Score) -> usize {
+    let mut result = 0;
+    for i in 0..5 {
+        let score = match s[i] {
+            Color::Black => 0,
+            Color::Yellow => 1,
+            Color::Green => 2,
+        };
+        result += 3_usize.pow(i.try_into().unwrap()) * score;
+    }
+    return result;
+}
+
 fn word_to_array(w: &str) -> Word {
     let char_vec: Vec<char> = w.chars().collect();
     let w2: Word = char_vec.try_into().expect("word does not have 5 chars");
@@ -49,50 +62,63 @@ fn load_words() -> Vec<Word> {
 }
 
 #[derive(Copy, Clone, Debug)]
-struct ExploreResult<'a> {
-    score: f64,
-    best_guess: &'a Word,
+struct ExploreResult {
+    score: f32,
+    best_guess: usize,
 }
 
-fn reduce_explore_results<'a>(a: ExploreResult<'a>, b: ExploreResult<'a>) -> ExploreResult<'a> {
+fn reduce_explore_results(a: ExploreResult, b: ExploreResult) -> ExploreResult {
     if a.score < b.score {
         a
     } else {
         b
     }
 }
-fn rate_guess<'a>(valid_words: &Vec<&'a Word>, guess: &'a Word) -> ExploreResult<'a> {
-    let mut all_scores = 0.;
-    for solution in valid_words.iter() {
+
+fn rate_guess(
+    pool: &Pool<Vec<usize>>,
+    n: usize,
+    all_scores: &Vec<Vec<usize>>,
+    remaining_words: &Vec<usize>,
+    guess: usize,
+) -> ExploreResult {
+    let mut score_sum = 0.;
+    for solution in remaining_words {
         if *solution == guess {
             // we found in 1
-            all_scores += 1.;
+            score_sum += 1.;
             continue;
         }
-        let score = rate_word(solution, guess);
-        let new_valid_words: Vec<&Word> = valid_words
-            .iter()
-            .filter(|w| rate_word(w, guess) == score)
-            .cloned()
-            .collect();
-        all_scores += 1. + explore(new_valid_words).score;
+        let score = all_scores[*solution][guess];
+        let mut new_valid_words: Vec<usize> = Vec::with_capacity(50);
+        for w in remaining_words {
+            if all_scores[*w][guess] == score {
+                new_valid_words.push(*w);
+            }
+        }
+        score_sum += 1. + explore(pool, n, all_scores, new_valid_words).score;
     }
     return ExploreResult {
         best_guess: guess,
-        score: all_scores / (valid_words.len() as f64),
+        score: score_sum / (remaining_words.len() as f32),
     };
 }
 
-fn explore<'a>(valid_words: Vec<&'a Word>) -> ExploreResult<'a> {
-    if valid_words.len() == 1 {
+fn explore(
+    pool: &Pool<Vec<usize>>,
+    n: usize,
+    all_scores: &Vec<Vec<usize>>,
+    remaining_words: Vec<usize>,
+) -> ExploreResult {
+    if remaining_words.len() == 1 {
         return ExploreResult {
             score: 1.,
-            best_guess: valid_words[0],
+            best_guess: remaining_words[0],
         };
     }
-    let best = valid_words
+    let best = remaining_words
         .iter()
-        .map(|guess| rate_guess(&valid_words, guess))
+        .map(|guess| rate_guess(pool, n, all_scores, &remaining_words, *guess))
         .reduce(reduce_explore_results);
     return best.unwrap();
 }
@@ -102,9 +128,9 @@ fn main() {
         panic!("expected pair number of arguments");
     }
     let mut words = load_words();
-    for n in 0..env::args().len()/2 {
-        let word = word_to_array(&env::args().nth(2*n + 1).unwrap());
-        let score_letters = word_to_array(&env::args().nth(2*n + 2).unwrap());
+    for n in 0..env::args().len() / 2 {
+        let word = word_to_array(&env::args().nth(2 * n + 1).unwrap());
+        let score_letters = word_to_array(&env::args().nth(2 * n + 2).unwrap());
         let score = score_letters.map(|c| match c {
             'B' => Color::Black,
             'b' => Color::Black,
@@ -112,17 +138,35 @@ fn main() {
             'g' => Color::Green,
             'Y' => Color::Yellow,
             'y' => Color::Yellow,
-            _ => panic!("score should contain B G or Y")
+            _ => panic!("score should contain B G or Y"),
         });
-        words = words.into_iter().filter(|w| rate_word(w, &word) == score).collect();
+        words = words
+            .into_iter()
+            .filter(|w| rate_word(w, &word) == score)
+            .collect();
     }
-    let words_refs = words.iter().collect();
-    let result = words
-        .par_iter()
-        .progress_count(words.len() as u64)
-        .map(|guess| rate_guess(&words_refs, guess))
-        .reduce_with(reduce_explore_results);
-    println!("{:?}", result);
+    let n = words.len();
+    let mut all_scores: Vec<Vec<usize>> = vec![vec![0; n]; n];
+    for i in 0..n {
+        for j in 0..n {
+            all_scores[i][j] = score_to_int(&rate_word(&words[i], &words[j]));
+        }
+    }
+
+    let pool: Pool<Vec<usize>> = Pool::new(2, || Vec::new());
+
+    let remaining_words: Vec<usize> = (0..n).collect();
+    let result = (0..n)
+        .into_par_iter()
+        .progress_count(n.try_into().unwrap())
+        .map(|i| rate_guess(&pool, n, &all_scores, &remaining_words, i))
+        .reduce_with(reduce_explore_results)
+        .unwrap();
+
+    println!(
+        "best word is {:?} for an avg score of {}",
+        words[result.best_guess], result.score
+    );
 }
 
 #[cfg(test)]
